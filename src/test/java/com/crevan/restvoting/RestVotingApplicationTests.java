f73fc1ab3fd98f8ec526109aa044aae01d6a4e49
Crevan <a.siverskii@gmail.com>
2023-04-13 10:18:38 +0300

    init commit

---
+++ src/test/java/com/crevan/restvoting/RestVotingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crevan.restvoting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestVotingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
