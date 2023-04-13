fa5157e9a8ed3453c9e63f192d8ff6abc9fda414
Crevan <a.siverskii@gmail.com>
2023-04-13 10:53:07 +0300

    [add] abstract BaseEntity model

---
+++ src/main/java/com/crevan/restvoting/model/BaseEntity.java
@@ -0,0 +1,46 @@
+package com.crevan.restvoting.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.util.ProxyUtils;
+
+@MappedSuperclass
+@Access(AccessType.FIELD)
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+public abstract class BaseEntity implements Persistable<Integer> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Integer id;
+
+    @Override
+    @JsonIgnore
+    public boolean isNew() {
+        return id == null;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass().equals(ProxyUtils.getUserClass(o))) {
+            return false;
+        }
+
+        BaseEntity that = (BaseEntity) o;
+
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id == null ? 0 : id;
+    }
+}
