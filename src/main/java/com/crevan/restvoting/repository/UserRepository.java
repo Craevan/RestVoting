9341931cd5ad8392d4b70772d0c681880c991bcf
Crevan <a.siverskii@gmail.com>
2023-04-17 20:40:31 +0300

    [update] add findByLastNameContainingIgnoreCase()

--- src/main/java/com/crevan/restvoting/repository/UserRepository.java
+++ src/main/java/com/crevan/restvoting/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package com.crevan.restvoting.repository;
 
 import com.crevan.restvoting.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RestResource;
@@ -14,4 +16,7 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     @RestResource(rel = "by-email", path = "by-email")
     @Query("SELECT u FROM User u WHERE u.email = LOWER(:email)")
     Optional<User> findByEmailIgnoreCase(final String email);
+
+    @RestResource(rel = "by-lastname", path = "by-lastname")
+    Page<User> findByLastNameContainingIgnoreCase(final String name, final Pageable pageable);
 }
