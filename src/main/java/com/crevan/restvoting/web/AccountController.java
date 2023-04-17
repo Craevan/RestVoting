7c92bc8c8c61318f4179ddc221843ed908279783
Crevan <a.siverskii@gmail.com>
2023-04-17 21:04:01 +0300

    [update] implementing RepresentationModelProcessor

--- src/main/java/com/crevan/restvoting/web/AccountController.java
+++ src/main/java/com/crevan/restvoting/web/AccountController.java
@@ -8,6 +8,11 @@ import com.crevan.restvoting.util.ValidationUtil;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.rest.webmvc.RepositoryLinksResource;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.server.RepresentationModelProcessor;
+import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -18,18 +23,29 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.Set;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
 @Slf4j
 @RestController
 @AllArgsConstructor
-@RequestMapping(value = "api/account")
-public class AccountController {
+@RequestMapping("api/account")
+public class AccountController implements RepresentationModelProcessor<RepositoryLinksResource> {
+
+    @SuppressWarnings("unchecked")
+    private static final RepresentationModelAssemblerSupport<User, EntityModel<User>> ASSEMBLER =
+            new RepresentationModelAssemblerSupport<>(AccountController.class, (Class<EntityModel<User>>) (Class<?>) EntityModel.class) {
+                @Override
+                public EntityModel<User> toModel(final User entity) {
+                    return EntityModel.of(entity, linkTo(AccountController.class).withSelfRel());
+                }
+            };
 
     private final UserRepository userRepository;
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public User get(@AuthenticationPrincipal final AuthUser authUser) {
+    @GetMapping(produces = MediaTypes.HAL_JSON_VALUE)
+    public EntityModel<User> get(@AuthenticationPrincipal final AuthUser authUser) {
         log.info("Get {}", authUser);
-        return authUser.getUser();
+        return ASSEMBLER.toModel(authUser.getUser());
     }
 
     @DeleteMapping
@@ -39,7 +55,7 @@ public class AccountController {
         userRepository.deleteById(authUser.id());
     }
 
-    @PostMapping(value = "/register", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<User> register(@Valid @RequestBody User user) {
         log.info("Register {}", user);
@@ -54,7 +70,7 @@ public class AccountController {
 
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void update(@Valid @RequestBody User user, @AuthenticationPrincipal final AuthUser authUser) {
+    public void update(@Valid @RequestBody final User user, @AuthenticationPrincipal final AuthUser authUser) {
         log.info("Update {} to {}", authUser, user);
         User oldUser = authUser.getUser();
         ValidationUtil.assureIdConsistent(user, oldUser.id());
@@ -63,5 +79,11 @@ public class AccountController {
             user.setPassword(oldUser.getPassword());
         }
         userRepository.save(user);
+    }
+
+    @Override
+    public RepositoryLinksResource process(final RepositoryLinksResource model) {
+        model.add(linkTo(AccountController.class).withRel("account"));
+        return model;
     }
 }
