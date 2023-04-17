46da20be5fd0b38908decd60f7cc1d75368b9fd1
Crevan <a.siverskii@gmail.com>
2023-04-17 15:50:12 +0300

    [update] add requestMatchers for route: "/api/account/register"

--- src/main/java/com/crevan/restvoting/config/WebSecurityConfig.java
+++ src/main/java/com/crevan/restvoting/config/WebSecurityConfig.java
@@ -41,6 +41,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .requestMatchers("/api/account/register").anonymous()
                 .requestMatchers("/api/account").hasRole(Role.USER.name())
                 .requestMatchers("/api/**").hasRole(Role.ADMIN.name())
                 .and()
