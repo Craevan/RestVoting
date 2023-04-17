154e90c9b143513ed3c25f2e31ce08809bb7b3f1
Crevan <a.siverskii@gmail.com>
2023-04-17 16:34:35 +0300

    [update] checkNew() and assureIdConsistent() throwing custom IllegalArgumentException

--- src/main/java/com/crevan/restvoting/util/ValidationUtil.java
+++ src/main/java/com/crevan/restvoting/util/ValidationUtil.java
@@ -1,12 +1,13 @@
 package com.crevan.restvoting.util;
 
+import com.crevan.restvoting.error.IllegalRequestDataException;
 import com.crevan.restvoting.model.BaseEntity;
 
 public class ValidationUtil {
 
     public static void checkNew(final BaseEntity entity) {
         if (!entity.isNew()) {
-            throw new IllegalArgumentException(entity + " must be new (id=null)");
+            throw new IllegalRequestDataException(entity.getClass().getSimpleName() + " must be new (id=null)");
         }
     }
 
@@ -14,7 +15,7 @@ public class ValidationUtil {
         if (entity.isNew()) {
             entity.setId(id);
         } else if (entity.id() != id) {
-            throw new IllegalArgumentException(entity + " must has id=" + id);
+            throw new IllegalRequestDataException(entity.getClass().getSimpleName() + " must has id=" + id);
         }
     }
 }
