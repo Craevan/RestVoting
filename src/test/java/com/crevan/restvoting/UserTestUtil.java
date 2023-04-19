1f8458fd708414b49580ebdff6ee79f9d2a6caab
Crevan <a.siverskii@gmail.com>
2023-04-19 19:25:16 +0300

    [update] add methods for getting new user and updated user

--- src/test/java/com/crevan/restvoting/UserTestUtil.java
+++ src/test/java/com/crevan/restvoting/UserTestUtil.java
@@ -1,8 +1,34 @@
 package com.crevan.restvoting;
 
+import com.crevan.restvoting.model.Role;
+import com.crevan.restvoting.model.User;
+
+import java.util.List;
+
 public class UserTestUtil {
     public static final int USER_ID = 1;
     public static final int ADMIN_ID = 2;
     public static final String USER_MAIL = "user@gmail.com";
     public static final String ADMIN_MAIL = "admin@javaops.ru";
+
+    public static User getNew() {
+        return new User(
+                null,
+                "new@gmail.com",
+                "New_First",
+                "New_Last",
+                "newpass",
+                List.of(Role.USER)
+        );
+    }
+
+    public static User getUpdated() {
+        return new User(USER_ID,
+                "user_update@gmail.com",
+                "User_First_Update",
+                "User_Last_Update",
+                "password_update",
+                List.of(Role.USER)
+        );
+    }
 }
