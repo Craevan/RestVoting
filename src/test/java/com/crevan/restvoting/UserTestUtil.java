cc2e57ed98cc7f559a4fbce94e9cb2c7301481c5
Crevan <a.siverskii@gmail.com>
2023-04-19 20:07:02 +0300

    [update] add expected users; add utility methods for tests

--- src/test/java/com/crevan/restvoting/UserTestUtil.java
+++ src/test/java/com/crevan/restvoting/UserTestUtil.java
@@ -2,14 +2,37 @@ package com.crevan.restvoting;
 
 import com.crevan.restvoting.model.Role;
 import com.crevan.restvoting.model.User;
+import com.crevan.restvoting.util.JsonUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultMatcher;
 
+import java.io.UnsupportedEncodingException;
 import java.util.List;
+import java.util.function.BiConsumer;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserTestUtil {
     public static final int USER_ID = 1;
     public static final int ADMIN_ID = 2;
     public static final String USER_MAIL = "user@gmail.com";
     public static final String ADMIN_MAIL = "admin@javaops.ru";
+
+    public static final User user = new User(USER_ID,
+            USER_MAIL,
+            "User_First",
+            "User_Last",
+            "password",
+            List.of(Role.USER)
+    );
+    public static final User admin = new User(ADMIN_ID,
+            ADMIN_MAIL,
+            "Admin_First",
+            "Admin_Last",
+            "admin",
+            List.of(Role.ADMIN, Role.USER)
+    );
 
     public static User getNew() {
         return new User(
@@ -30,5 +53,23 @@ public class UserTestUtil {
                 "password_update",
                 List.of(Role.USER)
         );
+    }
+
+    public static void assertEquals(User actual, User expected) {
+        assertThat(actual).usingRecursiveComparison().ignoringFields("password").isEqualTo(expected);
+    }
+
+    // No id in HATEOAS answer
+    public static void assertNoIdEquals(User actual, User expected) {
+        assertThat(actual).usingRecursiveComparison().ignoringFields("id", "password").isEqualTo(expected);
+    }
+
+    public static User asUser(MvcResult mvcResult) throws UnsupportedEncodingException, JsonProcessingException {
+        String jsonActual = mvcResult.getResponse().getContentAsString();
+        return JsonUtil.readValue(jsonActual, User.class);
+    }
+
+    public static ResultMatcher jsonMatcher(User expected, BiConsumer<User, User> equalsAssertion) {
+        return mvcResult -> equalsAssertion.accept(asUser(mvcResult), expected);
     }
 }
