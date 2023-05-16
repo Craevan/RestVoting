6791824d42af200bc4b6348fdde23b1d38329a8f
Crevan <a.siverskii@gmail.com>
2023-05-16 17:35:07 +0300

    [update] exclude starting h2Server from tests

--- src/main/java/com/crevan/restvoting/config/ApplicationConfig.java
+++ src/main/java/com/crevan/restvoting/config/ApplicationConfig.java
@@ -5,6 +5,7 @@ import org.h2.tools.Server;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.sql.SQLException;
 
@@ -13,6 +14,7 @@ import java.sql.SQLException;
 @Configuration
 public class ApplicationConfig {
 
+    @Profile("!test")
     @Bean(initMethod = "start", destroyMethod = "stop")
     public Server h2Server() throws SQLException {
         log.info("Starting H2 DB TCP Server");
